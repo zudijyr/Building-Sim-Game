4ce2934acd266aeb904744d8edcaf91e49a80a37
zydijyr <charliepye@gmail.com>
2015-10-31 16:43:24 -0700

    changes models back to tab indentation

--- sim/models/tile.py
+++ sim/models/tile.py
@@ -2,13 +2,13 @@ from . import terrain
 
 class Tile:
 
-    def __init__(self, terrain_type, x_position, y_position):
-        self.terrain_type = terrain_type
-        self.x_position = x_position
-        self.y_position = y_position
+	def __init__(self, terrain_type, x_position, y_position):
+		self.terrain_type = terrain_type
+		self.x_position = x_position
+		self.y_position = y_position
 
-    def displayTile(self):
-        print('terrain_type : ', self.terrain_type, 'x_position : ', self.x_position, 'y_position : ', self.y_position)
+	def displayTile(self):
+		print('terrain_type : ', self.terrain_type, 'x_position : ', self.x_position, 'y_position : ', self.y_position)
 
 tile1 = Tile(terrain.Grass, 1,2)
 assert(tile1.terrain_type.move_cost == 2)
